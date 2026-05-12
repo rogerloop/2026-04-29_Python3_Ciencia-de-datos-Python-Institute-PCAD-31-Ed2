dbf1aca40de57f80e3e574666b62edfc760f3521
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-05-12 18:01:30 +0200

    update

---
+++ 2026-05-12_SQL .sql
@@ -0,0 +1,16 @@
+-- con 2 guiones medios escribo un comentario
+-- SQL Server funciona Trasact-SQL es una version propia de Microsoft, ANSI-SQL es el estandar
+-- Trabajar con variables, con dicionales, con bucles, try/catch
+-- SQL = Consultar datos
+-- Con TRANSACT programar dentro del servidor
+-- TEnemos 2 tipos de sentencias
+-- DML --> Data Manipulation Language
+-- Select = selecciona datos pero no los modifica
+-- Insert into= Insertar datos
+-- Update = para modificar datos
+-- Daelete = para borra eliminar registros
+-- DDL  ->. Data DEfinition Language
+-- CREATE, ALTER, DROP, TRUNCATE, BK
+-- Ver los registros de la tabla pedidos
+
+SELECT * FROM Pedidos;
